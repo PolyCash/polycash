71984bff90ea2a6d8a9b5e49150241d61110c5f8
root <root@reprise4.openoffcampus.com>
2016-07-04 12:00:00 +0300

    Added wallet status include to the repository.

---
+++ includes/wallet_status.php
@@ -0,0 +1,6 @@
+<div style="overflow: hidden; margin-bottom: 10px;">
+	Account value:&nbsp;&nbsp;&nbsp;&nbsp;<font class="greentext"><?php echo number_format($account_value, 3); ?></font> EmpireCoins
+	<font style="float: right;">
+		Logged in as <?php echo $thisuser['username']; ?>. <a href="/wallet/?do=logout">Log Out</a>
+	</font>
+</div>
