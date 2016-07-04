2d7eb4972929d267c33654868321fd48a29e69f7
joeyfrich <joeyfrich@gmail.com>
2016-07-04 12:00:00 +0300

    Added code for adding new voting addresses automatically by cron.

--- cron/minutely.php
+++ cron/minutely.php
@@ -27,9 +27,35 @@ if ($_REQUEST['key'] != "" && $_REQUEST['key'] == $GLOBALS['cron_key_string']) {
 		}
 	}
 	
-	if ($GLOBALS['walletnotify_by_cron']) {
+	if ($GLOBALS['walletnotify_by_cron'] || $GLOBALS['min_unallocated_addresses'] > 0) {
 		$empirecoin_rpc = new jsonRPCClient('http://'.$GLOBALS['coin_rpc_user'].':'.$GLOBALS['coin_rpc_password'].'@127.0.0.1:'.$GLOBALS['coin_testnet_port'].'/');
-		
+	}
+	
+	if ($GLOBALS['min_unallocated_addresses'] > 0) {
+		$need_addresses = false;
+		$q = "SELECT * FROM nations WHERE nation_id > 1 ORDER BY nation_id ASC;";
+		$r = run_query($q);
+		while ($nation = mysql_fetch_array($r)) {
+			$qq = "SELECT COUNT(*) FROM addresses WHERE game_id='".$game['game_id']."' AND nation_id='".$nation['nation_id']."' AND user_id IS NULL;";
+			$rr = run_query($qq);
+			$num_addr = mysql_fetch_row($rr);
+			$num_addr = $num_addr[0];
+			if ($num_addr < $GLOBALS['min_unallocated_addresses']) {
+				$r = false;
+				$need_addresses = true;
+			}
+		}
+		if ($need_addresses) {
+			echo "Adding 500 addresses.<br/>\n";
+			for ($i=0; $i<500; $i++) {
+				$new_addr_str = $empirecoin_rpc->getnewaddress();
+				$new_addr_db = create_or_fetch_address($game, $new_addr_str, false, $empirecoin_rpc, true);
+			}
+		}
+		echo "Done generating addresses at ".round(microtime(true)-$script_start_time, 2)." seconds.<br/>\n";
+	}
+	
+	if ($GLOBALS['walletnotify_by_cron']) {
 		$q = "SELECT * FROM games WHERE game_id='".get_site_constant('primary_game_id')."';";
 		$r = run_query($q);
 		$game = mysql_fetch_array($r);
@@ -45,6 +71,8 @@ if ($_REQUEST['key'] != "" && $_REQUEST['key'] == $GLOBALS['cron_key_string']) {
 			die("An error occurred when attempting a coin RPC call.");
 		}
 	}
+	
+	echo "Script ran for ".round(microtime(true)-$script_start_time, 2)." seconds.<br/>\n";
 	/*
 	$q = "UPDATE users SET logged_in=0 WHERE last_active<".(time()-60*2).";";
 	$r = run_query($q);
