8ff7836ec6d98e178e492e5d6b5915773e9de0af
Joey Rich <joeyfrich@gmail.com>
2016-07-16 11:19:39 +0600

    Added game.send_round_notifications field.

--- sql/14.sql
+++ sql/14.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `users` DROP `notification_preference`;
 ALTER TABLE `user_games` ADD `notification_preference` ENUM('email','none') NOT NULL DEFAULT 'none' ;
+ALTER TABLE games ADD `send_round_notifications` TINYINT(1) DEFAULT 1;
