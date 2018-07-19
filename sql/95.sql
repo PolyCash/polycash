f548081f24d599ed2b0a2c745f6e894680eefcce
Joey Rich <joeyfrich@gmail.com>
2018-07-19 03:09:21 +0000

    Added event block indices for performance.

--- sql/95.sql
+++ sql/95.sql
@@ -2,3 +2,7 @@ ALTER TABLE `game_defined_events` ADD `espn_event_uid` VARCHAR(64) NULL DEFAULT 
 ALTER TABLE `game_defined_options` ADD `target_probability` FLOAT NULL DEFAULT NULL AFTER `name`;
 ALTER TABLE `blocks` ADD INDEX (`blockchain_id`, `time_mined`);
 ALTER TABLE `transactions` CHANGE `OP_RETURN` `OP_RETURN` TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL;
+ALTER TABLE `events` ADD INDEX (`game_id`, `event_starting_block`);
+ALTER TABLE `events` ADD INDEX (`game_id`, `event_final_block`);
+ALTER TABLE `options` DROP INDEX `event_option_index`;
+ALTER TABLE `options` ADD UNIQUE (`event_id`, `event_option_index`);
