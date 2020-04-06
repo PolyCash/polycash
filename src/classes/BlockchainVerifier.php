00e2482e258ee0679a46e6957de6537b605c3942
Joey Rich <joeyfrich@gmail.com>
2020-04-06 18:21:09 +0000

    Improvements to block verification for web api blockchains.

--- src/classes/BlockchainVerifier.php
+++ src/classes/BlockchainVerifier.php
@@ -5,7 +5,7 @@ class BlockchainVerifier {
 		$any_ios_in_error = false;
 		$any_ios_out_error = false;
 		
-		$tx_wrong_ios_in = $app->run_query("SELECT t.position_in_block, t.tx_hash, t.num_inputs, COUNT(*) FROM transaction_ios io JOIN transactions t ON io.spend_transaction_id=t.transaction_id WHERE t.block_id=:block_id AND t.blockchain_id=:blockchain_id GROUP BY io.spend_transaction_id HAVING COUNT(*) != t.num_inputs;", [
+		$tx_wrong_ios_in = $app->run_query("SELECT t.position_in_block, t.tx_hash, t.num_inputs, COUNT(*) FROM transactions t LEFT JOIN transaction_ios io ON io.spend_transaction_id=t.transaction_id WHERE t.block_id=:block_id AND t.blockchain_id=:blockchain_id AND t.transaction_desc != 'coinbase' GROUP BY io.spend_transaction_id HAVING COUNT(*) != t.num_inputs;", [
 			'block_id' => $block_id,
 			'blockchain_id' => $blockchain_id
 		]);
@@ -15,7 +15,7 @@ class BlockchainVerifier {
 			$any_ios_in_error = true;
 		}
 		
-		$tx_wrong_ios_out = $app->run_query("SELECT t.position_in_block, t.tx_hash, t.num_outputs, COUNT(*) FROM transaction_ios io JOIN transactions t ON io.create_transaction_id=t.transaction_id WHERE t.block_id=:block_id AND t.blockchain_id=:blockchain_id GROUP BY io.create_transaction_id HAVING COUNT(*) != t.num_outputs;", [
+		$tx_wrong_ios_out = $app->run_query("SELECT t.position_in_block, t.tx_hash, t.num_outputs, COUNT(*) FROM transactions t LEFT JOIN transaction_ios io ON io.create_transaction_id=t.transaction_id WHERE t.block_id=:block_id AND t.blockchain_id=:blockchain_id GROUP BY io.create_transaction_id HAVING COUNT(*) != t.num_outputs;", [
 			'block_id' => $block_id,
 			'blockchain_id' => $blockchain_id
 		]);
