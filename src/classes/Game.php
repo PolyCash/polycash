ef9231761c6ff7c555864527fc79707ccfa1d6ab
Joey Rich <joeyfrich@gmail.com>
2020-12-27 18:04:58 +0000

    Fix to game module loading related to recent changes.

--- src/classes/Game.php
+++ src/classes/Game.php
@@ -13,7 +13,7 @@ class Game {
 		
 		if (!empty($this->db_game['module'])) {
 			$module_class = $this->db_game['module'].'GameDefinition';
-			$module_fname = dirname(__DIR__)."/modules/".$module_class.".php";
+			$module_fname = dirname(__DIR__)."/modules/".$this->db_game['module']."/".$module_class.".php";
 			if (is_file($module_fname)) {
 				$this->module = new $module_class($this->blockchain->app);
 			}
