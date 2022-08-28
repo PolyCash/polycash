93b6d5648aff9f75aa7ce7744c0a68465e287aac
Joey Rich <joeyfrich@gmail.com>
2022-08-27 22:03:51 -0400

    Copy docker config file in entrypoint rather than Dockerfile.

--- run-apache.sh
+++ run-apache.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 CONF_FNAME=/root/.datacoin/datacoin.conf
-
 if ! [ -f "$CONF_FNAME" ]; then
 	cp /var/www/html/.dockerize/datachain/datacoin.conf "$CONF_FNAME"
+fi
+
+POLYCASH_CONF_FNAME=/var/www/html/src/config/config.json
+if ! [ -f "$POLYCASH_CONF_FNAME" ]; then
+	cp /var/www/html/.dockerize/polycash/example-config.json "$POLYCASH_CONF_FNAME"
 fi
 
 /var/www/html/datacoind &
