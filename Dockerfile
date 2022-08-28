144db70d649e2fec748a356aed1a86eb59985afb
Joey Rich <joeyfrich@gmail.com>
2022-08-27 22:14:08 -0400

    Download datacoin binaries in entrypoint rather than Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -23,12 +23,6 @@ RUN docker-php-ext-install pdo_mysql \
 COPY ./ ./
 RUN mv ./.dockerize/apache/000-default.conf /etc/apache2/sites-enabled/000-default.conf
 RUN mv ./.dockerize/cron/crontab /etc/crontab
-RUN mkdir /root/.datacoin
-
-RUN wget https://poly.cash/binaries/debian/datacoin-cli -P /var/www/html
-RUN wget https://poly.cash/binaries/debian/datacoind -P /var/www/html
-RUN chmod 755 /var/www/html/datacoind
-RUN chmod 755 /var/www/html/datacoin-cli
 
 RUN php /var/www/html/src/scripts/set_blockchain_parameters.php blockchain_identifier=datachain rpc_username=datacoinuser rpc_password=datacoinpass
 
--- run-apache.sh
+++ run-apache.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if ! [ -f "/root/.datacoin" ]; then
+	mkdir /root/.datacoin
+fi
+
 CONF_FNAME=/root/.datacoin/datacoin.conf
 if ! [ -f "$CONF_FNAME" ]; then
 	cp /var/www/html/.dockerize/datachain/datacoin.conf "$CONF_FNAME"
@@ -8,6 +12,16 @@ fi
 POLYCASH_CONF_FNAME=/var/www/html/src/config/config.json
 if ! [ -f "$POLYCASH_CONF_FNAME" ]; then
 	cp /var/www/html/.dockerize/polycash/example-config.json "$POLYCASH_CONF_FNAME"
+fi
+
+if ! [ -f "/var/www/html/datacoind" ]; then
+	wget https://poly.cash/binaries/debian/datacoind -P /var/www/html
+	chmod 755 /var/www/html/datacoind
+fi
+
+if ! [ -f "/var/www/html/datacoin-cli" ]; then
+	wget https://poly.cash/binaries/debian/datacoin-cli -P /var/www/html
+	chmod 755 /var/www/html/datacoin-cli
 fi
 
 /var/www/html/datacoind &
