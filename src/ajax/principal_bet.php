2a2510bc16ab8e5e2b08981638abecb11b789e50
Joey Rich <joeyfrich@gmail.com>
2023-02-08 21:34:18 -0500

    Resolved issue with wrong amount for bets submitted through principal betting UI.

--- src/ajax/principal_bet.php
+++ src/ajax/principal_bet.php
@@ -9,8 +9,8 @@ if ($thisuser && $game && $app->synchronizer_ok($thisuser, $_REQUEST['synchroniz
 	$amount = (float) $_REQUEST['amount'];
 	$option_id = (int) $_REQUEST['option_id'];
 	$fee = (float) $_REQUEST['fee'];
-	$fee_total = round($fee*pow(10, $game->blockchain->db_blockchain['decimal_places']));
-	$amount_total = round($amount*pow(10, $game->db_game['decimal_places']));
+	$fee_int = round($fee*pow(10, $game->blockchain->db_blockchain['decimal_places']));
+	$amount_int = round($amount*pow(10, $game->db_game['decimal_places']));
 	
 	$coins_per_vote = $app->coins_per_vote($game->db_game);
 	
@@ -30,14 +30,16 @@ if ($thisuser && $game && $app->synchronizer_ok($thisuser, $_REQUEST['synchroniz
 				
 				$spendable_ios_in_account = $app->spendable_ios_in_account($account['account_id'], $game->db_game['game_id'], $round_id, $last_block_id);
 				
-				$io_amount_sum = 0;
-				$game_amount_sum = 0;
+				$mandatory_bets = 0;
+				$io_amount_in = 0;
+				$game_amount_in = 0;
 				$io_ids = [];
+				$burn_game_amount = 0;
 				$keep_looping = true;
 				
 				while ($keep_looping && $io = $spendable_ios_in_account->fetch()) {
-					$game_amount_sum += $io['coins'];
-					$io_amount_sum += $io['amount'];
+					$game_amount_in += $io['coins'];
+					$io_amount_in += $io['amount'];
 					array_push($io_ids, $io['io_id']);
 					
 					if ($game->db_game['inflation'] == "exponential" && $game->db_game['exponential_inflation_rate'] > 0) {
@@ -47,52 +49,53 @@ if ($thisuser && $game && $app->synchronizer_ok($thisuser, $_REQUEST['synchroniz
 					}
 					else $this_mandatory_bets = 0;
 					
-					$game_amount_sum += $this_mandatory_bets;
+					$mandatory_bets += $this_mandatory_bets;
 					
-					if ($game_amount_sum >= $amount_total && $io_amount_sum >= $fee_total) $keep_looping = false;
+					$burn_game_amount = $amount_int-$mandatory_bets;
+					if ($game_amount_in >= $burn_game_amount*1.2) $keep_looping = false;
 				}
 				
 				$recycle_ios = $app->fetch_recycle_ios_in_account($user_game['account_id'], false);
 				
 				foreach ($recycle_ios as $recycle_io) {
 					array_push($io_ids, $recycle_io['io_id']);
-					$io_amount_sum += $recycle_io['amount'];
+					$io_amount_in += $recycle_io['amount'];
 				}
 				
-				if ($game_amount_sum >= $amount_total) {
-					$io_nonfee_amount = $io_amount_sum-$fee_total;
+				$io_nonfee_amount = $io_amount_in-$fee_int;
+				$game_coins_per_coin = $game_amount_in/$io_nonfee_amount;
+				
+				$burn_amount = ceil($burn_game_amount/$game_coins_per_coin);
+				
+				$io_nonfee_amount = $io_amount_in-$fee_int;
+				$io_nondestroy_amount = $io_nonfee_amount - $burn_amount;
+				
+				if ($io_nonfee_amount > 0) {
+					$io_separator_frac = 0.25;
+					$error_message = false;
 					
-					if ($io_nonfee_amount > 0) {
-						$destroy_frac = $amount_total/$game_amount_sum;
-						$destroy_io_amount = ceil($destroy_frac*$io_nonfee_amount);
-						$nondestroy_io_amount = $io_nonfee_amount-$destroy_io_amount;
-						$io_separator_frac = 0.25;
-						$error_message = false;
-						
-						$io_amounts = [$destroy_io_amount];
-						$address_ids = [$destroy_address['address_id']];
-						
-						$bet_addr_amount = $nondestroy_io_amount*(1-$io_separator_frac);
-						$separator_amount = $nondestroy_io_amount-$bet_addr_amount;
-						
-						array_push($io_amounts, $bet_addr_amount);
-						array_push($address_ids, $address['address_id']);
-						
-						array_push($io_amounts, $separator_amount);
-						array_push($address_ids, $separator_address['address_id']);
-						
-						$transaction_id = $game->blockchain->create_transaction("transaction", $io_amounts, false, $io_ids, $address_ids, $fee_total, $error_message);
-						
-						if ($transaction_id) {
-							$app->output_message(1, "Great, your transaction was submitted. <a href=\"/explorer/blockchains/".$game->blockchain->db_blockchain['url_identifier']."/transactions/".$transaction_id."/\">View Transaction</a>", false);
-						}
-						else {
-							$app->output_message(8, "TX Error: ".$error_message, false);
-						}
+					$io_amounts = [$burn_amount];
+					$address_ids = [$destroy_address['address_id']];
+					
+					$bet_addr_amount = round($io_nondestroy_amount*(1-$io_separator_frac));
+					$separator_amount = $io_nondestroy_amount-$bet_addr_amount;
+					
+					array_push($io_amounts, $bet_addr_amount);
+					array_push($address_ids, $address['address_id']);
+					
+					array_push($io_amounts, $separator_amount);
+					array_push($address_ids, $separator_address['address_id']);
+					
+					$transaction_id = $game->blockchain->create_transaction("transaction", $io_amounts, false, $io_ids, $address_ids, $fee_int, $error_message);
+					
+					if ($transaction_id) {
+						$app->output_message(1, "Great, your transaction was submitted. <a href=\"/explorer/blockchains/".$game->blockchain->db_blockchain['url_identifier']."/transactions/".$transaction_id."/\">View Transaction</a>", false);
 					}
-					else $app->output_message(7, "Not enough ".$game->blockchain->db_blockchain['coin_name_plural'].": ".$app->format_bignum($io_amount_sum/pow(10, $game->blockchain->db_blockchain['decimal_places']))." available.", false);
+					else {
+						$app->output_message(8, "TX Error: ".$error_message, false);
+					}
 				}
-				else $app->output_message(6, "You don't have enough coins to place this bet.", false);
+				else $app->output_message(7, "Not enough ".$game->blockchain->db_blockchain['coin_name_plural'].": ".$app->format_bignum($io_amount_in/pow(10, $game->blockchain->db_blockchain['decimal_places']))." available.", false);
 			}
 			else $app->output_message(5, "Transaction failed: no destroy address found in this account.", false);
 		}
