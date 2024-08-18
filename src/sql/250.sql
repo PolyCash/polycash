016c41c77c30c99923b12bd13757b42407145bb9
Joey Rich <joeyfrich@gmail.com>
2024-08-17 20:28:19 -0400

    Trying to make fetch game def faster.

--- src/sql/250.sql
+++ src/sql/250.sql
@@ -1,3 +1,4 @@
 ALTER TABLE options ADD COLUMN `op_event_index` INT(11) DEFAULT NULL AFTER `vote_identifier`;
 ALTER TABLE options ADD COLUMN `game_id` INT(11) DEFAULT NULL AFTER `event_id`;
 UPDATE options op JOIN events ev ON op.event_id=ev.event_id SET op.op_event_index=ev.event_index, op.game_id=ev.game_id;
+ALTER TABLE options ADD UNIQUE (`game_id`, `op_event_index`, `event_option_index`);
